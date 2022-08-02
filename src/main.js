219a3e0f5d248d01a0d4c812fda1464a18239d0f
calebnance <calebnance@gmail.com>
2022-08-01 22:06:43 -0400

    messing with goober for styling of components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { styled, setup } from "goober";
+
+setup(React.createElement);
+
+const Container = styled("div")`
+  background-color: #18a0fb;
+  font-weight: bold;
+`;
 
 const LineItem = ({ label, value }) => (
-  <div className="line-item">
-    <div>{label}</div>
-    <div>{value}</div>
-  </div>
+  <Container>
+    <div>Label: {label}</div>
+    <div>Value: {value}</div>
+  </Container>
 );
 
 LineItem.propTypes = {
