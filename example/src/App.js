053eba01eb9e943a6cc6908861a3bbda67008ba7
calebnance <calebnance@gmail.com>
2022-08-02 22:19:33 -0400

    sandbox react app for UI within /example/

---
+++ example/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from 'figma-plugin-ui';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1>figma-plugin-ui components</h1>
+        <h2>Buttons</h2>
+        <Button />
+        <Button type="primary" />
+    </div>
+  );
+}
+
+export default App;
